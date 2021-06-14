1c4057ab7b5cd9ed3954e6f43086ee300a136904
billj290 <billj290@gmail.com>
2021-06-14 17:05:21 +0800

    while set done

--- password.py
+++ password.py
@@ -1,10 +1,14 @@
-password = input('請輸入密碼: ')
-if password == 'a123456':
-		print('登入成功!')
-if password != 'a123456':
-	y = 2
-	while y > 0:
-		print('密碼錯誤, 還有', y, '次機會')
-		input('請輸入密碼: ')
-		y = y - 1
-		
+psw = 'a123456'
+y = 3 #能夠嘗試的機會
+while y > 0:
+	y = y - 1
+	x = input('請輸入密碼: ')
+	if x != psw:
+		print('密碼錯誤')
+		if y > 0:
+			print('尚有', y, '次機會')
+		else:
+			print('沒機會鎖帳號!')
+	else:
+		print('登入成功')
+		break
